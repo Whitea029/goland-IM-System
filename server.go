7ac144f0bc5a4e7873a2534bf9aa3331c1f6831c
Whitea029 <1664915116@qq.com>
2024-08-23 14:05:17 +0800

    feat: 用户上线

--- server.go
+++ server.go
@@ -3,16 +3,29 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+
+	// 在线用户的列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	// 消息广播的channel
+	Message chan string
 }
 
 // 创建一个server接口
 func NewServer(ip string, port int) *Server {
-	server := &Server{ip, port}
+	server := &Server{
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
+	}
 	return server
 }
 
@@ -25,6 +38,9 @@ func (this *Server) Start() {
 		return
 	}
 	defer listen.Close()
+
+	go this.ListenMassage()
+
 	for {
 		// accept
 		conn, err := listen.Accept()
@@ -36,11 +52,36 @@ func (this *Server) Start() {
 		go this.Handle(conn)
 	}
 
-	// close listen socket
+}
 
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	this.Message <- sendMsg
 }
 
 func (this *Server) Handle(conn net.Conn) {
 	// 当前连接的业务
 	fmt.Println("连接建立成功")
+	user := NewUser(conn)
+	// 用户上线
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+	// 广播上线消息
+	this.BroadCast(user, "已上线")
+	// 当前handel阻塞
+	select {}
+}
+
+// 监听Message的goroutine， 一旦有消息就会发给所有在线的User
+func (this *Server) ListenMassage() {
+	for {
+		msg := <-this.Message
+
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
 }
